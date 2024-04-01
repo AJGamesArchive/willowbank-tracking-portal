49e59c4065dc20d5605993d772d27c6995eb4066
AJGamesArchive <68122156+AJGamesArchive@users.noreply.github.com>
2024-04-01 21:01:57 +0100

    [DEV] Added Client Activity Data As TS Objects

--- src/types/Activity.ts
+++ src/types/Activity.ts
@@ -1,6 +1,13 @@
+// ADT declaration for a collection of activities
 export type Activities = {
     [key: number]: {
         description: string;
         xpValue: number;
     };
+};
+
+// Type declaration for an individual activity
+export type Activity = {
+    description: string;
+    xpValue: number;
 };
