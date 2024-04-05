fdef32e47f7b6aa0db4a1a2684dab9f5d2cd4e65
AJGamesArchive <68122156+AJGamesArchive@users.noreply.github.com>
2024-04-05 16:23:38 +0100

    Fixed Issue With Loading Environment Variables

--- src/database/config/FirestoreConfig.ts
+++ src/database/config/FirestoreConfig.ts
@@ -1,11 +1,20 @@
+// Import and type cast env variables
+const apiKey: string = import.meta.env.APIKEY;
+const authDomain: string = import.meta.env.AUTHDOMAIN;
+const projectId: string = import.meta.env.PROJECTID;
+const storageBucket: string = import.meta.env.STORAGEBUCKET;
+const messagingSenderId: string = import.meta.env.MESSAGINGSENDERID;
+const appId: string = import.meta.env.APPID;
+const measurementId: string = import.meta.env.MEASUREMENTID;
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: import.meta.env.APIKEY,
-  authDomain: import.meta.env.AUTHDOMAIN,
-  projectId: import.meta.env.PROJECTID,
-  storageBucket: import.meta.env.STORAGEBUCKET,
-  messagingSenderId: import.meta.env.MESSAGINGSENDERID,
-  appId: import.meta.env.APPID,
-  measurementId: import.meta.env.MEASUREMENTID
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+// Define data types for .env variables
+interface ImportMetaEnv {
+  APIKEY: string;
+  AUTHDOMAIN: string;
+  PROJECTID: string;
+  STORAGEBUCKET: string;
+  MESSAGINGSENDERID: string;
+  APPID: string;
+  MEASUREMENTID: string;
+};
